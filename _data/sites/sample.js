e00cd64a4234d09c8a197597ba8bc75433a71cba
Khoirul Aksara <57844594+khoirulaksara@users.noreply.github.com>
2023-01-03 10:51:35 +0700

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,11 @@
 module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
+	skip: false
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://mojo.my.id/"
 	]
 };
