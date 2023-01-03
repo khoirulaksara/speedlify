d3b5fe8c7c287ce4c4796d89f42d6eae1bfd9a18
Khoirul Aksara <57844594+khoirulaksara@users.noreply.github.com>
2023-01-03 11:01:34 +0700

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
+	skip: false,
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
